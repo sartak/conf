fc70a0e219613df4af9316d723599a0056af4b4f
Shawn M Moore <sartak@gmail.com>
2009-12-24 16:39:23 -0500

    The undo persistence patch now applies to the whole tree

--- rebuild-vim.sh
+++ rebuild-vim.sh
@@ -2,9 +2,7 @@ cd ~/app/vim7
 make distclean
 svn revert --recursive *
 svn up
-cd src
 curl http://jordanlewis.org/code/persistent_undo.diff | patch -p0
-cd ..
 ./configure --without-x --disable-gui --disable-xim --disable-darwin --enable-perlinterp --enable-multibyte
 make
 sudo make install
