c950531247bf8756adff4f5e08df620816f948f5
Shawn M Moore <sartak@bestpractical.com>
2010-08-20 22:06:20 -0400

    Update rebuild-vim.sh for 7.3/hg

--- rebuild-vim.sh
+++ rebuild-vim.sh
@@ -1,9 +1,11 @@
-cd ~/devel/vim7
+cd ~/devel/vim
+
 make distclean
-svn revert --recursive *
-svn up
-curl http://jordanlewis.org/code/persistent_undo.diff | patch -p0
+hg pull
+hg update -C
+
 ./configure --without-x --disable-gui --disable-xim --disable-darwin --enable-perlinterp --enable-multibyte
 make
+
 sudo make install
 
