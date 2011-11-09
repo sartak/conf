df6e17e96acac8864b8e90038c31cd773164a014
Shawn M Moore <sartak@gmail.com>
2011-11-09 11:19:28 -0500

    Build python into vim, for Gundo

--- rebuild-vim.sh
+++ rebuild-vim.sh
@@ -4,7 +4,7 @@ make distclean
 hg pull
 hg update -C
 
-./configure --without-x --disable-gui --disable-xim --disable-darwin --enable-perlinterp --enable-rubyinterp --enable-multibyte
+./configure --without-x --disable-gui --disable-xim --disable-darwin --enable-perlinterp --enable-rubyinterp --enable-pythoninterp --enable-multibyte
 make
 
 sudo make install
