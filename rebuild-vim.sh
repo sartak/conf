cb8e1ed521ddd962a0b970109fbc79c5458ef971
Shawn M Moore <code@sartak.org>
2012-04-30 15:04:28 -0400

    Stop building python into vim

--- rebuild-vim.sh
+++ rebuild-vim.sh
@@ -4,7 +4,7 @@ make distclean
 hg pull
 hg update -C
 
-./configure --without-x --disable-gui --disable-xim --disable-darwin --enable-perlinterp --enable-rubyinterp --enable-pythoninterp --enable-multibyte
+./configure --without-x --disable-gui --disable-xim --disable-darwin --enable-perlinterp --enable-rubyinterp --enable-multibyte
 make
 
 sudo make install
