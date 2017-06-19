cb0eb8054034a78d19e3f46779a63eae4b10cb18
Shawn M Moore <code@sartak.org>
2017-06-19 19:46:56 -0400

    Improve prompt

--- config.fish
+++ config.fish
@@ -12,3 +12,41 @@ set fish_greeting ""
 function jnethack
     cocot -t UTF-8 -p EUC-JP -- jnethack $argv;
 end
+
+set __fish_git_prompt_show_informative_status 'yes'
+set __fish_git_prompt_char_dirtystate '⚡'
+set __fish_git_prompt_char_stagedstate '→'
+
+function fish_prompt
+    set -l last_status $status
+
+    set -l host (hostname | sed "s/\.local//")
+    switch $host
+      case giedi-prime
+        set_color B300FF
+      case gammu buzzell junction tleilax
+        set_color FFB400
+      case booth morelax bloc
+        set_color 00A6ED
+      case vagrant
+        set_color 2E00E6
+      case '*'
+        set_color F6511D
+    end
+
+    echo -n (whoami)'@'$host' '
+
+    set_color $fish_color_cwd
+    echo -n (prompt_pwd)
+
+    set_color -o yellow
+    echo -n (__fish_git_prompt)
+
+    set_color normal
+
+    if not [ $last_status -eq 0 ]
+        set_color $fish_color_error
+    end
+    echo -n ' $ '
+end
+
