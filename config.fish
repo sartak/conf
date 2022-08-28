27eeb2281eda04abaf48c0b3a3c21bbf2631edf1
Shawn M Moore <code@sartak.org>
2022-08-28 12:46:36 -0400

    Rg in vim

--- config.fish
+++ config.fish
@@ -84,10 +84,9 @@ function f --wraps=fzf --description 'fzf'
   fzf $argv
 end
 
-set -x FZF_DEFAULT_OPTS '--cycle --layout=reverse --border --height=90% --preview-window=down:75%,wrap,border-horizontal --marker="*" --color=bg+:#000000,border:#222222,fg+:#F8F8F2,preview-bg:#000000,info:italic:#5f5f5f,bg:#000000,spinner:#A6E22E,gutter:#000000,prompt:#A6E22E,preview-fg:#F8F8F2,fg:#F8F8F2,disabled:#000000,header:#F8F8F2,marker:#F92672,hl+:#ffd700,query:#5fd7ff,hl:#ffd700,pointer:#5fd7ff'
+set -x FZF_DEFAULT_OPTS '--cycle --layout=reverse --border --height=90% --preview-window=down:75%,wrap,border-horizontal --marker="*" --color=bg+:#000000,border:#222222,fg+:#F8F8F2,preview-bg:#000000,info:italic:#5f5f5f,bg:#000000,spinner:#A6E22E,gutter:#000000,prompt:#A6E22E,preview-fg:#F8F8F2,fg:#F8F8F2,disabled:#5fd7ff,header:#F8F8F2,marker:#F92672,hl+:#ffd700,query:#5fd7ff,hl:#ffd700,pointer:#5fd7ff'
 
 set -x RIPGREP_CONFIG_PATH ~/.config/ripgreprc
 
 # this has to go late as well
 bind \cv edit_command
-
--- vim/bundle/sartak/plugin/fzf.vim
+++ vim/bundle/sartak/plugin/fzf.vim
@@ -15,3 +15,12 @@ let g:fzf_layout = { 'window': { 'width': 0.9, 'height': 0.9 } }
 let g:fzf_preview_window = ['down:75%,wrap,border-horizontal', 'ctrl-/']
 let g:coc_fzf_preview = 'down:75%,wrap,border-horizontal'
 let g:coc_fzf_opts = []
+
+function! RipgrepFzf(query, fullscreen)
+  let command_fmt = 'rg --no-heading --color=always -- %s'
+  let initial_command = printf(command_fmt, shellescape(a:query))
+  let reload_command = printf(command_fmt, '{q}')
+  let spec = {'options': ['--phony', '--query', a:query, '--bind', 'change:reload:'.reload_command]}
+  call fzf#vim#grep(initial_command, 1, fzf#vim#with_preview(spec), a:fullscreen)
+endfunction
+command! -nargs=* -bang Rg call RipgrepFzf(<q-args>, <bang>0)
