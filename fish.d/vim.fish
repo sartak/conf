dbcf8381cddc8a9e01e3830d6ae1c13dd6369b8a
Shawn M Moore <code@sartak.org>
2022-08-28 20:43:15 -0400

    Session management

--- fish.d/vim.fish
+++ fish.d/vim.fish
@@ -26,3 +26,44 @@ function edit_command
   set -xl VISUAL vi
   edit_command_buffer
 end
+
+function vs --description 'launch vim session'
+  set newsession 0
+
+  if not set -q argv[1]
+    set exa_opts -1 --sort accessed --reverse ~/.vim/sessions/
+    set session (
+      exa $exa_opts | fzf --prompt="vim session > " --exit-0 --header \e"[38;5;240;3mctrl-x to delete" --preview-window='bottom:50%' --info=hidden --preview='cd ~/.vim/sessions/; stat -f "Accessed: %Sa%nModified: %Sm%nCreated:  %SB" {}; echo; set_color 23A5FA; g --color=never "^cd (.+)" -r \'$1\' {}; set_color -o normal; g --color=never "^badd \+\d+ (.+)" -r \'  - $1\' {}' --bind "ctrl-x:execute-silent(rm ~/.vim/sessions/{})+reload(exa $exa_opts)"
+    )
+    if test $status -ne 0
+        set session (
+          exa ~/.vim/sessions/ | head -n1
+        )
+        if test "$session" = ""
+          echo "no sessions exist; run `vs <session>` or `\\sc` in vim to create" 1>&2
+        end
+      return
+    end
+  else if test "$argv[1]" = "-"
+    set session (
+      exa -1 --sort modified --reverse ~/.vim/sessions/ | head -n1
+    )
+    if test "$session" = ""
+      echo "no sessions exist; run `vs <session>` or `\\sc` in vim to create" 1>&2
+      return
+    end
+  else
+    set session $argv[1]
+    if not test -e "$HOME/.vim/sessions/$argv[1]"
+      set newsession 1
+    end
+  end
+
+  set -l VIM (which vim)
+
+  if test $newsession -eq 1
+    $VIM -c ":silent SessionSaveAs $session"
+  else
+    $VIM -c ":silent SessionOpen $session"
+  end
+end
---
+++ vim/bundle/sartak/plugin/sessions.vim
@@ -0,0 +1,6 @@
+set sessionoptions-=blank sessionoptions-=options sessionoptions+=tabpages
+
+nnoremap <leader>so :SessionOpen<CR>
+nnoremap <leader>sc :SessionSave<CR>
+nnoremap <leader>sx :SessionClose<CR>
+nnoremap <leader>ss :SessionList<CR>
