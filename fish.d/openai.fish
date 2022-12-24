ef56a88b832c91e9abb2780d2a10a7e7c9ada670
Shawn M Moore <code@sartak.org>
2022-12-24 15:14:17 -0500

    Add qf and qqf for asking followup question

--- fish.d/openai.fish
+++ fish.d/openai.fish
@@ -1,8 +1,5 @@
 function qraw --description 'ask openai'
   set prompt $argv[1]
-  echo $prompt
-  return
-
   echo "$prompt" > ~/.local/share/openai/prompt
 
   if not set -q tokens
@@ -35,7 +32,33 @@ function q --description 'ask openai'
   qraw "$prompt"
 end
 
+function qf --description 'add question to current openai thread'
+  if not set -q argv[1]
+    echo 'usage: qf question…' >&2
+    return 1
+  end
+
+  set context (command cat ~/.local/share/openai/prompt)
+  set new (echo $argv | jq -R)
+
+  set prefix (string sub --length 1 $context)
+
+  if test "$prefix" = '"'
+    set prompt "[$context, $new]"
+  else
+    set prompt (string replace -r ']$' ", $new]" $context)
+  end
+
+  qraw "$prompt"
+end
+
 function qq --description 'ask openai but longer'
   set -fx tokens 1000
   q $argv
 end
+
+function qqf --description 'add question to current openai thread but longer'
+  set -fx tokens 1000
+  qf $argv
+end
+
