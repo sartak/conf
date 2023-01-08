c9791515bca5675b9d081b7dfa5cecbff54401d0
Shawn M Moore <code@sartak.org>
2023-01-08 01:06:16 -0500

    Add z.

--- fish.d/z.fish
+++ fish.d/z.fish
@@ -7,3 +7,13 @@ function z- --wraps='z -' --description 'z -'
     return 1
   end
 end
+
+function z. --wraps='z ..' --description 'z ..'
+  if not set -q argv[1]
+    z ..
+  else
+    set_color -o yellow
+    echo "this command takes no args" 1>&2
+    return 1
+  end
+end
