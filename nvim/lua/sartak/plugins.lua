58a7a7dfe6a62167d19be434d60710cdaf8e55e5
Shawn M Moore <code@sartak.org>
2023-12-24 16:05:44 -0500

    Add and configure monokai-pro

--- nvim/lua/sartak/plugins.lua
+++ nvim/lua/sartak/plugins.lua
@@ -1,5 +1,28 @@
 require("lazy").setup({
   {
+    'loctvl842/monokai-pro.nvim',
+    lazy = false,
+    priority = 1000,
+    opts = {
+      transparent_background = true,
+      filter = "classic",
+      styles = {
+        comment = { italic = true },
+        keyword = { italic = false },
+        type = { italic = false },
+        storageclass = { italic = false },
+        structure = { italic = false },
+        parameter = { italic = false },
+        annotation = { italic = false },
+        tag_attribute = { italic = false },
+      },
+    },
+    config = function(_, opts)
+      require("monokai-pro").setup(opts)
+      vim.cmd.colorscheme("monokai-pro")
+    end
+  },
+  {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.5',
     dependencies = { 'nvim-lua/plenary.nvim' },
