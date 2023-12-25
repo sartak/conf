7bbf9ddfd9e6ba1fea6526fc466a237834362219
Shawn M Moore <code@sartak.org>
2023-12-24 22:22:47 -0500

    Add more lsp servers

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -4,4 +4,19 @@ lsp_zero.on_attach(function(client, bufnr)
   lsp_zero.default_keymaps({buffer = bufnr})
 end)
 
+require('lspconfig').bashls.setup({})
+require('lspconfig').cssls.setup({})
+require('lspconfig').dockerls.setup({})
+require('lspconfig').jsonls.setup({})
 require('lspconfig').rust_analyzer.setup({})
+require('lspconfig').tailwindcss.setup({})
+require('lspconfig').tsserver.setup({})
+
+require('lspconfig').eslint.setup({
+  on_attach = function(client, bufnr)
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
+      command = "EslintFixAll",
+    })
+  end,
+})
