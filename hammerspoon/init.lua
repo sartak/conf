9419413953be0cfa4a854d70e814e2eaad09046e
Shawn M Moore <code@sartak.org>
2024-12-25 11:07:07 -0500

    Remove unused hammerspoon

--- hammerspoon/init.lua
+++
@@ -1,9 +0,0 @@
-local VimMode = hs.loadSpoon('VimMode')
-local vim = VimMode:new()
-
-vim:bindHotKeys({ enter = {{'ctrl'}, ';'} })
-
-vim:useFallbackMode('Google Chrome')
-vim:disableForApp('iTerm')
-
-vim:enableBetaFeature('block_cursor_overlay')
