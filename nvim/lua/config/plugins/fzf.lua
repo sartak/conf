16f580b271b5cffbeef32337e1a1b052c965be9c
Shawn M Moore <code@sartak.org>
2025-01-04 13:07:13 -0500

    Improve fzf display

--- nvim/lua/config/plugins/fzf.lua
+++ nvim/lua/config/plugins/fzf.lua
@@ -5,6 +5,25 @@ return {
       defaults = {
         header = false,
       },
+      winopts = {
+        height = 0.9,
+        width = 0.9,
+        row = 0.2,
+        col = 0.5,
+        preview = {
+          vertical = 'down:75%',
+          layout = 'vertical',
+          title = false,
+          -- https://github.com/ibhagwan/fzf-lua/issues/1668
+          border = 'border-top',
+          winopts = {
+            number = false,
+          },
+        },
+      },
+      grep = {
+        prompt = "rg> ",
+      },
     },
   },
 }
