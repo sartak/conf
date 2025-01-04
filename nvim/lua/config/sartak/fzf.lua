2658497ba95a4833463aa0ad28900d7121dbc6c7
Shawn M Moore <code@sartak.org>
2025-01-04 12:22:09 -0500

    Add ^G for livegrep in vim

--- nvim/lua/config/sartak/fzf.lua
+++ nvim/lua/config/sartak/fzf.lua
@@ -1,1 +1,2 @@
 vim.keymap.set({ "n", "v", "i" }, "<C-t>", function() require("fzf-lua").files() end, { silent = true })
+vim.keymap.set({ "n", "v", "i" }, "<C-g>", function() require("fzf-lua").live_grep_native() end, { silent = true })
