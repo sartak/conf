7bb821ec02e4b34c19b764c29a6d1e4d30e6df94
Shawn M Moore <code@sartak.org>
2025-01-04 14:09:01 -0500

    Syntax highlight parent commit in gitcommit message

---
+++ nvim/after/queries/gitcommit/highlights.scm
@@ -0,0 +1,13 @@
+; extends
+
+((comment) @keyword
+           (#match? @keyword "^# Parent: [0-9a-f]{7} .*$")
+           (#offset! @keyword 0 2 0 0))
+
+((comment) @constant
+           (#match? @constant "^# Parent: [0-9a-f]{7} .*$")
+           (#offset! @constant 0 10 0 0))
+
+((comment) @string
+           (#match? @string "^# Parent: [0-9a-f]{7} .*$")
+           (#offset! @string 0 18 0 0))
