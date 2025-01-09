0e816593e5437325128ad7a621b6bdbecc250fee
Shawn M Moore <code@sartak.org>
2025-01-09 16:25:24 -0500

    Add conform, have it be responsible for formatting

---
+++ nvim/lua/config/plugins/conform.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    'stevearc/conform.nvim',
+    opts = {
+      format_on_save = {
+        lsp_format = "fallback",
+      },
+    },
+  }
+}
--- nvim/lua/config/plugins/lsp.lua
+++ nvim/lua/config/plugins/lsp.lua
@@ -61,22 +61,6 @@ return {
       require("lspconfig").ts_ls.setup {
         capabilities = capabilities,
       }
-
-      vim.api.nvim_create_autocmd('LspAttach', {
-        callback = function(args)
-          local client = vim.lsp.get_client_by_id(args.data.client_id)
-          if not client then return end
-
-          if client.supports_method('textDocument/formatting') then
-            vim.api.nvim_create_autocmd('BufWritePre', {
-              buffer = args.buf,
-              callback = function()
-                vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
-              end
-            })
-          end
-        end
-      })
     end,
   },
 }
