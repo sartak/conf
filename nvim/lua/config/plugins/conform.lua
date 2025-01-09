97842c48372a40036d4e0875fcd4bbc0b24fbc5c
Shawn M Moore <code@sartak.org>
2025-01-09 17:42:23 -0500

    Add conform config for erb_lint

--- nvim/lua/config/plugins/conform.lua
+++ nvim/lua/config/plugins/conform.lua
@@ -3,7 +3,19 @@ return {
     'stevearc/conform.nvim',
     opts = {
       format_on_save = {
+        timeout_ms = 10000,
         lsp_format = "fallback",
+      },
+      formatters_by_ft = {
+        eruby = { "erb_lint" },
+      },
+      formatters = {
+        erb_lint = {
+          stdin = false,
+          tmpfile_format = ".conform.$RANDOM.$FILENAME",
+          command = "bundle",
+          args = { "exec", "erb_lint", "--autocorrect", "--fail-level", "f", "$FILENAME" },
+        },
       },
     },
   }
